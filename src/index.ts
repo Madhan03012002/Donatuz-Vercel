3a54280d4c3796b5beb9d90531dcf46a75871b37
Madhan03012002 <madhankumar51172@gmail.com>
2025-02-08 14:11:41 +0530

    backend code teste1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,53 @@
-import express from "express"
+// import express from "express"
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/",(req,res)=>{
+//     res.status(200).send("Hello from the server")
+// })
+
+// const port = process.env.PORT || 8000;
+// app.listen(port,()=>{
+//     console.log(`listening the port ${port}`)
+// })
+import express from 'express';
+import BodyParser from 'body-parser';
 const app = express();
 
-app.use(express.json());
-
-app.get("/",(req,res)=>{
-    res.status(200).send("Hello from the server")
-})
-
-const port = process.env.PORT || 8000;
-app.listen(port,()=>{
-    console.log(`listening the port ${port}`)
-})
+const init = () => {
+    try {
+  
+      const PORT = process.env.PORT || 3000;
+      app.use(BodyParser.json());
+    //   app.use(BodyParser.json({ limit: '50mb' }))
+      app.all("/*", function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Content-Type, Authorization, Content-Length, X-Requested-With"
+        );
+        res.header("Access-Control-Allow-Credentials", "true");
+        next();
+      });
+  
+      //api
+      app.get("/",(req,res)=>{
+        res.status(200).send("Hello from the server")
+    })
+      app.get("/bookcalls", (req, res) => {
+        res.send(
+          `<h1 style='text-align:center;'>🤞➖Welcome to Booking Calls ➖🤞</h1><br><h2 style='text-align:center;'>${new Date().toLocaleString()}</h2>`
+        );
+      });
+  
+      app.listen(PORT, () => {
+        console.log(`🚀 Port is running Successfully in ${PORT}`);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  init();
+ 
